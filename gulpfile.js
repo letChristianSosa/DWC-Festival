bd24ba69b67348bf40131c6a37b07e51d40d0350
letChristianSosa <chrissm28@outlook.com>
2022-05-01 03:16:50 -0500

    Completed LineUp Section

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo .scss a compilar
@@ -10,10 +15,34 @@ function css(done) {
   done();
 }
 
+function imageMin(done) {
+  src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin({ optimizationLevel: 3 })))
+    .pipe(dest("build/img"));
+  done();
+}
+
+function webpVersion(done) {
+  src("src/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 50 }))
+    .pipe(dest("build/img"));
+  done();
+}
+
+function avifVersion(done) {
+  src("src/img/**/*.{png,jpg}")
+    .pipe(avif({ quality: 50 }))
+    .pipe(dest("build/img"));
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imageMin = imageMin;
+exports.webpVersion = webpVersion;
+exports.avifVersion = avifVersion;
+exports.dev = parallel(imageMin, webpVersion, avifVersion, dev);
